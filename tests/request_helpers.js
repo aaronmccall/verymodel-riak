9d759fbd91f9f54c588fc3403c88b39ff3c1973d
Aaron McCall <aaron@andyet.net>
2014-01-30 16:14:15 -0800

    cleanup request_helper tests

--- tests/request_helpers.js
+++ tests/request_helpers.js
@@ -9,51 +9,72 @@ module.exports = {
         cb();
     },
     'equivalent get calls return properly': function (test) {
-        var key = 'foobarbazbiz',
-            strings = helpers.get(this.model, [key]),
-            str_and_obj = helpers.get(this.model, [{key: key}]);
-        test.ok(_.isEqual(strings, str_and_obj), 'strings and str_and_obj are NOT the same' + JSON.stringify([strings, str_and_obj], null, 2));
+        var key = 'foobarbazbiz';
+        test.ok(
+            _.isEqual(helpers.get(this.model, [key]), helpers.get(this.model, [{key: key}])),
+            'strings and str_and_obj are NOT the same'
+        );
         test.done();
     },
     "equivalent index exact match calls return properly": function (test) {
-        var index = 'foobar',
-            key = 'bazbiz',
-            strings = helpers.index(this.model, [index, key]),
-            str_and_obj = helpers.index(this.model, [{index: index, key: key}]);
-        test.ok(_.isEqual(strings, str_and_obj), 'strings and str_and_obj are NOT the same');
+        var index = 'subject',
+            key = 'math:algebra';
+        test.ok(
+            _.isEqual(
+                helpers.index(this.model, [index, key]),
+                helpers.index(this.model, [{index: index, key: key}])
+            ),
+            'strings and str_and_obj are NOT the same'
+        );
         test.done();
     },
     "equivalent index range calls return properly": function (test) {
-        var index = 'foobar',
-            min = 'bazbiz',
-            max = 'zibzab',
-            strings = helpers.index(this.model, [index, min, max]),
-            str_and_obj = helpers.index(this.model, [{index: index, range_min: min, range_max: max}]);
-        test.ok(_.isEqual(strings, str_and_obj), 'strings and str_and_obj are NOT the same');
+        var index = 'grade_level',
+            min = 1,
+            max = 5;
+        test.ok(
+            _.isEqual(
+                helpers.index(this.model, [index, min, max]),
+                helpers.index(this.model, [{index: index, range_min: min, range_max: max}])
+            ),
+            'strings and str_and_obj are NOT the same'
+        );
         test.done();
     },
     "equivalent search calls return properly": function (test) {
         var index = 'foobar',
-            q = 'name:bazbiz',
-            strings = helpers.search(this.model, [index, q]),
-            str_and_obj = helpers.search(this.model, [{index: index, q: q}]);
-        test.ok(_.isEqual(strings, str_and_obj), 'strings and str_and_obj are NOT the same');
+            q = 'name:bazbiz';
+        test.ok(
+            _.isEqual(
+                helpers.search(this.model, [index, q]),
+                helpers.search(this.model, [{index: index, q: q}])
+            ),
+            'strings and str_and_obj are NOT the same'
+        );
         test.done();
     },
     "equivalent mapreduce calls return properly": function (test) {
         var index = 'foobar',
             key = 'bazbiz',
-            query = [],
-            strings = helpers.mapreduce(this.model, [index, key, query]),
-            str_and_obj = helpers.mapreduce(this.model, [{ inputs: { index: index, key: key}, query: query}]);
-        test.ok(_.isEqual(strings, str_and_obj), 'strings and str_and_obj are NOT the same');
+            query = [];
+        test.ok(
+            _.isEqual(
+                helpers.mapreduce(this.model, [index, key, query]),
+                helpers.mapreduce(this.model, [{ inputs: { index: index, key: key}, query: query}])
+            ),
+            'strings and str_and_obj are NOT the same'
+        );
         test.done();
     },
     "equivalent del calls return properly": function (test) {
-        var key = 'foobarbazbiz',
-            strings = helpers.del(this.model, [key]),
-            str_and_obj = helpers.del(this.model, [{key: key}]);
-        test.ok(_.isEqual(strings, str_and_obj), 'strings and str_and_obj are NOT the same' + JSON.stringify([strings, str_and_obj], null, 2));
+        var key = 'foobarbazbiz';
+        test.ok(
+            _.isEqual(
+                helpers.del(this.model, [key]),
+                helpers.del(this.model, [{key: key}])
+            ),
+            'strings and str_and_obj are NOT the same'
+        );
         test.done();
     }
 };
